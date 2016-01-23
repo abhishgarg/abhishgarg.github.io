5dd25b8dcc7d25e4c8b5f7b6964d9a6b042a11ea
Abhishek Garg <abhishek.garg@wingify.com>
2016-01-24 01:43:33 +0530

    fix some typo and errors

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
       $('.content-wrapper').addClass('animated slideInRight')
     } else {
       $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '33%'}, 400, swing = 'swing', function () {})
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
 
@@ -23,7 +23,7 @@ $(document).ready(function () {
         $('.content-wrapper').addClass('animated slideInRight')
       } else {
         $('.panel-cover').css('max-width', currentWidth)
-        $('.panel-cover').animate({'max-width': '530px', 'width': '33%'}, 400, swing = 'swing', function () {})
+        $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
       }
     })
 
